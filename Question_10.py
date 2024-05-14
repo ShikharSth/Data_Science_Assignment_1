281c27bbf52d1a477db5a668e90572719c0780ab
ShikharSth <151653474+ShikharSth@users.noreply.github.com>
2024-05-14 17:33:08 +0545

    Create Question_10.py

---
+++ Question_10.py
@@ -0,0 +1,6 @@
+# Create a dictionary containing the names of fruits as keys and their corresponding colors as values and print it. Add a new fruit-color pair to the dictionary and print the updated dictionary.
+
+dictionary_fruit = {"strawberry":"red", "orange":"orange", "mango":"yellow"}
+dictionary_fruit
+dictionary_fruit.update({"grapes":"purple"})
+dictionary_fruit
