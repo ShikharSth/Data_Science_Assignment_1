b8c15bc65af0b8eadfd070a10f57f48f5b7943aa
ShikharSth <151653474+ShikharSth@users.noreply.github.com>
2024-05-14 15:39:33 +0545

    Create Question_5.py

---
+++ Question_5.py
@@ -0,0 +1,5 @@
+# Create a list of integers from 1 to 10 and print it. Calculate the sum of all the numbers in the list and print the result. 
+list_int = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list_int
+sum = sum(list_int)
+sum
