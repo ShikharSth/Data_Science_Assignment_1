2b6e2737887a181d8f942c73ccb8fd077a2ca1d3
ShikharSth <151653474+ShikharSth@users.noreply.github.com>
2024-05-14 16:39:55 +0545

    Create Question_9.py

---
+++ Question_9.py
@@ -0,0 +1,6 @@
+# Create a dictionary representing the information of a book with keys "title", "author", and "year" and print it. Update the year of the book and print the updated dictionary.
+
+dictionary_book = {"title":"Ugly Love", "author":"Colleen Hoover", "year":"2014"}
+dictionary_book
+dictionary_book["year"] = 2015
+dictionary_book
