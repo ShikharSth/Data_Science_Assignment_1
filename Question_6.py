f329e7421d1aa53b8e23951a2113b505bcd52b30
ShikharSth <151653474+ShikharSth@users.noreply.github.com>
2024-05-14 15:49:20 +0545

    Create Question_6.py

---
+++ Question_6.py
@@ -0,0 +1,6 @@
+# Create a list of mixed data types including strings, integers, and floats and print it. Remove the first element from the list and print the modified list.
+
+list_mixed = ['hello', 'world', 1, 2, 3, 5.5, 3.5]
+list_mixed
+list_mixed.pop(0)
+list_mixed
