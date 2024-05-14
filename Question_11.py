5f99f6872f5b8254b34ad880c1321c370fc1a5f4
ShikharSth <151653474+ShikharSth@users.noreply.github.com>
2024-05-14 17:38:59 +0545

    Create Question_11.py

---
+++ Question_11.py
@@ -0,0 +1,6 @@
+# Create a dictionary representing the population of different cities with keys as city names and values as population numbers and print it. Remove a city from the dictionary and print the modified dictionary.
+
+dictionary_city = {"bhaktapur":"240000", "kathmandu":"350000", "lalitpur":"300000","tokyo":"500000"}
+dictionary_city
+dictionary_city.pop('tokyo')
+dictionary_city
