070f09ccab9ad7aa667353112ea8c63b354d8db9
ShikharSth <151653474+ShikharSth@users.noreply.github.com>
2024-05-14 15:33:27 +0545

    Update Question_4.py

--- Question_4.py
+++ Question_4.py
@@ -1,3 +1,4 @@
+# Create a list of your favorite movies and print it. Then, append a new movie to the list and print the updated list.
 list_a = ['spider man', 'harry potter', 'RRR']
 list_a
 list_a.append('KGF')
